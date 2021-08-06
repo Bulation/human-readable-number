496cf07a6e38328be176f30114afd4e420669806
Bulation <insanbulat@gmail.com>
2021-08-06 17:13:55 +0300

    feat: solve

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function toReadable (number) {
   let arr = [
       ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'],
-      ['ten', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'],
+      ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'],
       ['one hundred', 'two hundred', 'three hundred', 'four hundred', 'five hundred', 'six hundred', 'seven hundred', 'eight hundred', 'nine hundred']
   ] 
   let str = '';
@@ -11,7 +11,7 @@ module.exports = function toReadable (number) {
   while (num.length > 0) {
       if (num[0] == '0')
       {
-        num.unshift();
+        num.shift();
         continue;
       }  
       if (+(num.join('')) >= 0 && +(num.join('')) <= 20) {
